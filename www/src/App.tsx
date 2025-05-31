221d8bca221b92a484ce08d64c57672f33dcb83a
501A <art71903design@gmail.com>
2025-05-30 19:36:54 -0700

    Update HTML meta tags and remove `react-helmet`

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,49 +1,34 @@
 import IconGrid from "./IconGrid";
 import { version } from "shisa-icon/package.json";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <title>Shisa Iconography</title>
-        <meta name="description" content="A collection of icons." />
-        <meta property="og:title" content="Shisa Iconography" />
-        <meta property="og:description" content="A collection of icons." />
-        <meta property="og:image" content="/og.png" />
-        <meta property="og:url" content="https://501A.vercel.app" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Shisa Iconography" />
-        <meta name="twitter:description" content="A collection of icons." />
-        <meta name="twitter:image" content="/og.png" />
-      </Helmet>
-      <main className="w-full min-h-screen flex flex-col items-center justify-between">
-        <header className="mx-auto max-w-96 py-12 grid gap-6">
-          <hgroup className="text-center grid gap-3">
-            <h1>Shisa Iconography</h1>
-            <h4>A collection of icons.</h4>
-          </hgroup>
-          <div className="flex items-center justify-center gap-6">
-            <a href="https://www.npmjs.com/package/shisa-icon">NPM</a>
-            <a href="https://github.com/501A-gh/shisa-icon">GitHub</a>
-          </div>
-        </header>
-        <IconGrid />
-        <footer className="mx-auto max-w-96 py-12 text-center grid gap-6">
-          <p>Version {version}</p>
-          <p>
-            Designed by{" "}
-            <a
-              href="https://501A.vercel.app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              501A
-            </a>
-          </p>
-        </footer>
-      </main>
-    </>
+    <main className="w-full min-h-screen flex flex-col items-center justify-between">
+      <header className="mx-auto max-w-96 py-12 grid gap-6">
+        <hgroup className="text-center grid gap-3">
+          <h1>Shisa Iconography</h1>
+          <h4>A collection of icons.</h4>
+        </hgroup>
+        <div className="flex items-center justify-center gap-6">
+          <a href="https://www.npmjs.com/package/shisa-icon">NPM</a>
+          <a href="https://github.com/501A-gh/shisa-icon">GitHub</a>
+        </div>
+      </header>
+      <IconGrid />
+      <footer className="mx-auto max-w-96 py-12 text-center grid gap-6">
+        <p>Version {version}</p>
+        <p>
+          Designed by{" "}
+          <a
+            href="https://501A.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            501A
+          </a>
+        </p>
+      </footer>
+    </main>
   );
 }
 
